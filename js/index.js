5d5bc7839de5ba7e143867fd579b46e56759efe1
AndresRamirez917 <andresmintic2021@gmail.com>
2024-08-28 17:43:25 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
-    const services_row = document.querySelector('.services-row')
-
-    async function getData(){
-        const result = await fetch('https://jsonplaceholder.typicode.com/photos/')
-        const photos = await result.json()
-        photos.forEach(element => {
-            if(element.id <= 3){
-                const card = document.createRange().createContextualFragment(`
+async function getData(){
+    const result = await fetch('http://jsonplaceholder.typicode.com/photos');
+    const photos = await result.json();
+    photos.forEach(element => {
+        if(element.id <=3){
+            const card = document.createRange().createContextualFragment(`
                     <div class="card">                
-                        <img src="${element.url}" alt="">
-                        <p>In ipsum ac tincidunt volutpat lacinia. Egestas quis neque, volutpat elementum. Scelerisque massa aliquam tellus sit nibh. Dignissim ipsum viverra id cursus adipiscing.</p>                
-                     </div>    
-                    `)
-                    services_row.append(card)
-            }
-        });
-    }
-    
-    getData()
-   
+                        <p class="idtitle-json">${element.id}.<span>${element.title}</span></p>
+                         <img src="${element.url}" alt="">
+                         <p>In ipsum ac tincidunt volutpat lacinia. Egestas quis neque, volutpat elementum. Scelerisque massa aliquam tellus sit nibh. Dignissim ipsum viverra id cursus adipiscing.</p>                
+                    </div> 
+                `)
+                const services_row = document.querySelector('.services-row');
+                services_row.append(card)
+        }
+    });
+}
+
+getData()
